7b19cac18f9e1094581730ae8bc55dc73c7a17d4
Ghanshyam Mishra <ghanshyammishra3205615@gmail.com>
2022-09-20 14:17:39 +0530

    first commit

---
+++ src/main/java/com/ghanshyam/httpserver/HttpserverApplication.java
@@ -0,0 +1,12 @@
+package com.ghanshyam.httpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpserverApplication.class, args);
+	}
+}
---
+++ src/main/java/com/ghanshyam/httpserver/handlers/Me.java
@@ -0,0 +1,36 @@
+package com.ghanshyam.httpserver.handlers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class Me {
+
+    @GetMapping("/me")
+    public String me() {
+        return "Just another sexy boi";
+    }
+
+    @GetMapping("/name" +
+            "")
+    public String name() {
+        return "Ghanshyam Mishra";
+    }
+
+
+
+//    @RequestMapping(value = "/link", method = RequestMethod.GET)
+//    public void method(HttpServletResponse httpServletResponse) {
+//        httpServletResponse.setHeader("Location", "https://www.youtube.com/watch?v=gr6QnFmCXzk&ab_channel=HAUSER");
+//        httpServletResponse.setStatus(302);
+//    }
+
+    @GetMapping("/age")
+    public String age() {
+        return "20";
+    }
+}
